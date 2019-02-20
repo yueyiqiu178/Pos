897a4c3df11112fd0e9c06c2d8713423db75a4d9
YueyiQiu <yueyiqiu100@gmail.com>
2019-02-20 17:51:14 +0800

    調整專案架構

--- src/index.js
+++ src/index.js
@@ -15,19 +15,35 @@ import {
     Switch
   } from 'react-router-dom'
 
+// ReactDOM.render(
+
+// <Router>
+// <Login>
+//   <Switch>
+//     <Route exact path="/" component={APP}/>
+//     <Route path="/helloworld" component={APP}/>
+//     <Route component={Menu}/>
+//   </Switch>
+// </Login>
+// </Router>
+
+// , document.getElementById('root'));
+
 ReactDOM.render(
+  <Provider store={store}>
+      <Router history={browserHistory}>
+          <Route path="/" component={App}>
+              <IndexRoute component={Login}/>
+              <Route path="home" component={Home}/>
+              <Route path="login" component={Login}/>
+              <Route path="register" component={Register}/>
+              <Route path="wall/:username" component={Wall}/>
+          </Route>
+      </Router>
+  </Provider>, rootElement
+);
 
-<Router>
-<Login>
-  <Switch>
-    <Route exact path="/" component={APP}/>
-    <Route path="/helloworld" component={APP}/>
-    <Route component={Menu}/>
-  </Switch>
-</Login>
-</Router>
 
-, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
